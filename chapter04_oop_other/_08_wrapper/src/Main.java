b5d65de0e2a83a0f733bfc179cad738d5102c7ed
马中豪 <mazhonghao@mazhonghaodeMacBook-Pro.local>
2024-04-03 10:30:40 +0800

    20240403 commit

--- chapter04_oop_other/_08_wrapper/src/Main.java
+++ chapter04_oop_other/_08_wrapper/src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import org.junit.Test;
 
 import static java.lang.Integer.valueOf;
@@ -37,4 +38,17 @@ public class Main {
 
     }
 
+=======
+public class Main {
+    public static void main(String[] args) {
+        // 自动装箱底层会调用valueOf()
+        Integer i = 1;
+        Integer j = 1;
+        System.out.println(i == j); // ture
+
+        Integer m = 127;
+        Integer n = 127;
+        System.out.println(m == n); // false
+    }
+>>>>>>> 9f68438 (2023.4.1 commit)
 }
